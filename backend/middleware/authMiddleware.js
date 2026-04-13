3edff3a50620435d5b0797043404ba8862451d13
Aditya <adityaalba27@gmail.com>
2026-04-13 19:46:14 +0530

    adding authmiddleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.status(401).json({message:"Authorization header missing or invalid"});
+    }
+    const token = authHeader.split(" ")[1];
+    try{
+
+        if(!token){
+            return res.status(401).json({message:"Token is missing"})
+        }
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    }
+    catch(error){
+        console.log(error);
+        return res.status(401).json({message:"Invalid or Expired Token"})
+    }
+}
